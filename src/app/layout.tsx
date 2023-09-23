1302d54d8e8db4a8f3d14be6c8a03eb99c9b8ddb
diovanedm <47329665+diovanedm@users.noreply.github.com>
2023-09-23 10:30:59 -0300

    header added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import './globals.css'
 import type { Metadata } from 'next'
 import { Poppins } from 'next/font/google'
 
-const poppins = Poppins({ subsets: ['latin'], weight: ['300', '400', '500', '600'] })
+const poppins = Poppins({ subsets: ['latin'], weight: ['300', '400', '500', '600'], variable: '--font-poppins' })
 
 export const metadata: Metadata = {
   title: 'Create Next App',
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,36 @@
 import Image from 'next/image'
+import Avatar from '@/assets/avatar.svg'
+import Wave from '@/assets/wave.svg'
 
 export default function Home() {
   return (
-    <h1>Hello world</h1>
+
+    <div className='flex justify-center items-center min-h-screen'>
+      <div>
+        <header>
+          <div className='flex flex-row gap-2'>
+            <div className='bg-gray-200 w-16 h-16 rounded-full flex justify-center items-center'>
+              <Image
+                src={Avatar}
+                alt="Avatar"
+                className='w-14 h-14'
+              />
+            </div>
+
+            <div>
+              <p className='text-white text-2xl font-light'>Boa tarde</p>
+              <div className=' flex flex-row gap-2 items-center'>
+                <p className='text-white text-2xl font-semibold'>Diovane</p>
+                <Image
+                  src={Wave}
+                  alt="Wave"
+                  className='w-6 h-6'
+                />
+              </div>
+            </div>
+          </div>
+        </header>
+      </div>
+    </div >
   )
 }
