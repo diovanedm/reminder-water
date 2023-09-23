984d1930f78f9bed6eae07926035e1aae112412b
diovanedm <47329665+diovanedm@users.noreply.github.com>
2023-09-23 10:52:56 -0300

    water added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,14 @@
 import Image from 'next/image'
 import Avatar from '@/assets/avatar.svg'
 import Wave from '@/assets/wave.svg'
+import Water from '@/assets/water.svg'
 
 export default function Home() {
   return (
 
     <div className='flex justify-center items-center min-h-screen'>
       <div>
-        <header>
+        <header className='pb-20'>
           <div className='flex flex-row gap-2'>
             <div className='bg-gray-200 w-16 h-16 rounded-full flex justify-center items-center'>
               <Image
@@ -30,6 +31,23 @@ export default function Home() {
             </div>
           </div>
         </header>
+
+        <div className='flex'>
+          <div className='bg-blue-300 rounded-3xl w-52 px-11 py-8 flex flex-col items-center'>
+            <p className='font-extrabold text-xs text-gray-500'>80%</p>
+
+            <Image
+              src={Water}
+              alt={'Water'}
+              height={150}
+              className='mt-10 mb-8'
+            />
+
+            <p className='font-semibold text-xl text-gray-500'>Beber água</p>
+            <p className='font-medium text-base text-gray-500'>Meta: 300ml</p>
+
+          </div>
+        </div>
       </div>
     </div >
   )
